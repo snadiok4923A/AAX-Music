de010d1856d3f33ae431b0d22fe396bdb103676c
Sandipan Paul <107307131+snadiok4923A@users.noreply.github.com>
2026-04-24 00:16:33 +0530

    Add files via upload

---
+++ playlists.js
@@ -0,0 +1,94 @@
+// playlists.js
+
+const playlists = {
+	relax: [
+		{
+			title: "Shanti",
+			artist: "",
+			src: "relax/songs/1.mp3",
+			cover: "relax/images/1.jpg",
+		},
+		{
+			title: "Prashant",
+			artist: "",
+			src: "relax/songs/2.mp3",
+			cover: "relax/images/2.jpg",
+		},
+		{
+			title: "Shaant",
+			artist: "",
+			src: "relax/songs/3.mp3",
+			cover: "relax/images/3.jpg",
+		},
+		{
+			title: "Nirmal",
+			artist: "",
+			src: "relax/songs/4.mp3",
+			cover: "relax/images/4.jpg",
+		},
+		{
+			title: "Sukoon",
+			artist: "",
+			src: "relax/songs/5.mp3",
+			cover: "relax/images/5.jpg",
+		},
+	],
+
+	youtube: [
+		{
+			title: "Mohon Ke Darshan",
+			src: "yt/songs/Mohon Ke Darshan.mp3",
+			cover: "yt/images/1.jpg",
+		},
+		{
+			title: "Tu Aur May Remix",
+			src: "yt/songs/Tu Aur May Remix.mp3",
+			cover: "yt/images/2.jpg",
+		},
+		{
+			title: "Sath Chale Reverb",
+			src: "yt/songs/Sath Chale Reverb.mp3",
+			cover: "yt/images/3.jpg",
+		},
+		{
+			title: "Tu Aur Main",
+			src: "yt/songs/Tu Aur Main.mp3",
+			cover: "yt/images/4.jpg",
+		},
+		{
+			title: "Hare Krishna 2.0",
+			src: "yt/songs/hare krishna 2.0.mp3",
+			cover: "yt/images/5.jpg",
+		},
+	],
+
+	techno: [
+		{
+			title: "Bass Drop",
+			src: "techno/songs/1.mp3",
+			cover: "techno/images/1.jpg",
+		},
+		{
+			title: "Bass Drop",
+			src: "techno/songs/2.mp3",
+			cover: "techno/images/2.jpg",
+		},
+		{
+			title: "Bass Drop",
+			src: "techno/songs/3.mp3",
+			cover: "techno/images/3.jpg",
+		},
+		{
+			title: "Bass Drop",
+			src: "techno/songs/4.mp3",
+			cover: "techno/images/4.jpg",
+		},
+		{
+			title: "Bass Drop",
+			src: "techno/songs/5.mp3",
+			cover: "techno/images/5.jpg",
+		},
+	],
+};
+
+window.playlists = playlists;
